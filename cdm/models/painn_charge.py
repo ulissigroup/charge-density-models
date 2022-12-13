f0770fe4cc00f07d05131efa105d4332d2e33ec0
Ethan Sunshine <93541000+emsunshine@users.noreply.github.com>
2022-12-13 15:50:02 -0500

    Remove pdb import

--- cdm/models/painn_charge.py
+++ cdm/models/painn_charge.py
@@ -3,8 +3,6 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 """
 
-import pdb
-
 from ocpmodels.common.registry import registry
 import torch
 from ocpmodels.models.painn.painn import PaiNN
