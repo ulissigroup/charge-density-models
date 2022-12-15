6fd1ce9036eb042cfd09d59d6e7f08b6eeb18c08
Ethan Sunshine <esunshin@andrew.cmu.edu>
2022-12-15 15:48:17 +0000

    Add GemNet-OC

--- cdm/models/__init__.py
+++ cdm/models/__init__.py
@@ -1,5 +1,6 @@
 from .schnet_charge import schnet_charge
 from .gemnet_charge import GemNetT_charge
+from .gemnet_oc_charge import GemNet_OC_charge
 from .painn_charge import PaiNN_Charge
 from .scn_charge import SCN_Charge
 from .chargemodel import ChargeModel
